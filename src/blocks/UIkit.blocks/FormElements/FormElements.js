4c6230ba0dbdb93d3ba9996ad3aea3c0620bf31d
Ray <ray-227@ya.ru>
2020-12-08 00:25:47 +0300

    Сделал Masked Text Field.
    .input-date-masked, по данному селектору построен весь функционал и стили.
    
    Вводить можно только 10 чисел, если ввели больше 10 чисел, букву или символ,
    тогда поле подсветится красным.
    
    Стоит валидация даты, но без учета колличества дней в текущем месяце, валидация
    месяца от 01 до 12, год должен начинаться с 20 и любые два числа.
    
    Для проверки есть методы у класса MaskedDate:
      1) MaskedDate.is: вернет true, если дата прошла валидацию, иначе false;
      2) MaskedDate.get: вернет дату, если дата прошла валидацию, иначе null.

--- src/blocks/UIkit.blocks/FormElements/FormElements.js
+++ src/blocks/UIkit.blocks/FormElements/FormElements.js
@@ -1,4 +1,4 @@
-if( document.querySelector('.dropdown') ) {
+if ( document.querySelector('.dropdown') ) {
 
   class DropDown {
     constructor() {
@@ -96,4 +96,60 @@ if( document.querySelector('.dropdown') ) {
       dropDown.toggle();
     }
   });
+}
+
+if ( document.querySelector('.input-date-masked') ) {
+
+  class MaskedDate {
+    constructor() {
+      this.regExpDate = new RegExp('(0[1-9]|[1-2][0-9]|3[0-1])[.](0[1-9]|1[0-2])[.](20\\d{2})');
+    }
+
+    is(element) {
+      return this.regExpDate.test(element.value);
+    }
+
+    get(element) {
+      return this.regExpDate.exec(element.value);
+    }
+  }
+
+  let inputDateMasked = document.querySelectorAll('.input-date-masked');
+  let maskedDate = new MaskedDate();
+
+  inputDateMasked.forEach( (item) => {
+
+    item.oninput = item.onkeydown = (e) => {
+
+      if (item.value.length > 10) {
+        item.value = item.value.substr(0, 10);
+        item.classList.add('input_error');
+        setTimeout( () => item.classList.remove('input_error'), 300);
+      } else if (
+        !Number(item.value[item.value.length - 1])
+        &&
+        Number(item.value[item.value.length - 1]) !== 0
+        &&
+        item.value.length !== 0
+        ) {
+        item.value = item.value.substr(0, item.value.length - 1);
+        item.classList.add('input_error');
+        setTimeout( () => item.classList.remove('input_error'), 300);
+      } else {
+        item.classList.remove('input_error');
+      }
+  
+      if ( (item.value.length === 2 || item.value.length === 5) && e.code !== 'Backspace' ) {
+        item.value += '.';
+      }
+  
+      // Данная проверка показательная, чтобы просто была, данную проверку нужно делать только при отправке на сервер.
+      if(!maskedDate.is(item) && item.value.length === 10) {
+        item.classList.add('input_error');
+      }
+
+    };
+
+  });
+
 }
