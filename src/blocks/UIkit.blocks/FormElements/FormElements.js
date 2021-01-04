be3a9699e0be269cf0f0f55822c4af5512a4dd92
Ray <ray-227@ya.ru>
2021-01-04 20:38:42 +0300

    Сделал pagination.
    Теперь нужно доделать range, а именно изменение атрибутов inpunt=range и вывод
    чисел пользователю.

--- src/blocks/UIkit.blocks/FormElements/FormElements.js
+++ src/blocks/UIkit.blocks/FormElements/FormElements.js
@@ -310,21 +310,84 @@ if ( document.querySelector('.range-slider__input') ) {
       // Конец движения ползунков
       let rangeEnd = Math.round( ( (range.offsetWidth - leftThump.offsetWidth) / range.offsetWidth) * 100 ) + 0.5;
 
-      function posRangeFill(leftThumpBound, rightThumpBound, whichThump) {
-        if (whichThump === 'left') {
-          rangeFill.style.left = (leftThumpBound - move + 2) + '%';
-          rangeFill.style.width = (rightThumpBound - leftThumpBound + move) + '%';
-        } else if (whichThump === 'right') {
-          rangeFill.style.left = (leftThumpBound) + '%';
-          rangeFill.style.width = (rightThumpBound - leftThumpBound + move + 2) + '%';
-        }
-      }
-
       eventThump(leftThump, 'left');
       eventThump(rightThump, 'right');
       
-      // ВЕШАЕМ СОБЫТИЯ
+      /* 
+        ВЕШАЕМ СОБЫТИЯ - первый вариант реализаций.
+
+        У меня есть еще две задумки:
+          1)Выставлять valueLeft или valueRight в % от max в зависимости сколько % у style left.
+            Формула valueLeft = currentThump.style.left / 100 * max:
+              valueLeft = 50 / 100 * 20000
+              valueLeft = 0.5 * 20000
+              valueLeft = 10000
+          2)Изменять значение valueLeft или valueRight кадждый раз, когда ползунок пройдет определенный %.
+            Находим сколько нужно пройти ползунку, чтобы изменить значение:
+              Формула move = (step / max) * 100:
+                move = (this.options.step / this.options.max) * 100;
+                move = (100 / 20000) * 100;
+                move = 0.5;
+            Примерная логика:
+              if (event.clientX === move) {
+                valueLeft = currentThump.style.left / 100 * max;
+              }
+
+        В первом варианте нельзя выставить step, valueLeft или valueRight будут изменяться, каждое движение ползунка.
+
+        Во втором варианте можно указать, когда изменять valueLeft или valueRight, значение будет изменяться, только
+        когда ползунок пройдет определенный %, который задан в move. Данный вариант я еще не обдумал полностью.
+
+        TODO: Как реализовать второй вариант?
+          Я могу каждый раз, когда event.clientX достигает отметки в move, то есть event.clientX === move,
+          делаеть move += move, таким образом следующая отметка увеличиться на шаг move.
+          Теперь нужно подумать как уменьшать move. Я могу проверять, если event.clientX равен прошлому шагу,
+          то есть move-move, if (event.clientX === move-move), тогда move -= move.
+          
+          Но не ясно, что если move напримрем 0.5, а то и меньше, что если резко сдвинуть ползунок?
+          Мне нужно как-то расчитывать значение move в зависимости от того где сейчас ползунок.
+          
+          Допустим у нас range шириной 1000px, max=100, step=1.
+          Если резко дернуть ползунок, пусть мы попадем на 48% от ширины range.
+
+          Полузнок на 48%, сколько это step?
+          Нам нужно узнать сколько процентов от max в step. step = 1 / 100 * 100, step это 1% от max.
+          Полузнок = 48%, step = 1%.
+
+          Сколько нужно шагов, чтобы step стал равен 48%?
+          48 / 1 = 48 шагов.
+          step = 48%. 
+
+          Проведем эти же расчеты, но где max=10000, step=5, thump=34%;
+          step = 5 / 10000 * 100, step = 0,05%
+          Сколько нужно шагов, чтобы step стал 34% от 10000, то есть 3400?
+          step = 34 / 0.05, step = 680
+
+          Запутался отложу расчеты уже 4 утра.
+      */
       function eventThump(currentThump, whichThump) {
+      /*
+        Функция для определение ширины и положения полоски между левым и правым ползунком.
+
+        Если ширина range маленька, а max число большое и step имеет большой шаг,
+        тогда зеленая градиентовая полоса между левым и правым ползунком ломается.
+
+        Я пытался сделать, чтобы все корректно работало при step 1000,
+        у меня получилось сделать, чтобы с левым ползунком ничего не ломалось,
+        но не получилось решить поломку с правым ползунком.
+
+        Думаю стоит просто ограничить step.
+      */
+        function posRangeFill(leftThumpBound, rightThumpBound, whichThump) {
+          if (whichThump === 'left') {
+            rangeFill.style.left = (leftThumpBound - move + 2) + '%';
+            rangeFill.style.width = (rightThumpBound - leftThumpBound + move) + '%';
+          } else if (whichThump === 'right') {
+            rangeFill.style.left = (leftThumpBound) + '%';
+            rangeFill.style.width = (rightThumpBound - leftThumpBound + move + 2) + '%';
+          }
+        }
+
         currentThump.onmousedown = function(event) {
           event.preventDefault();
           
@@ -348,7 +411,6 @@ if ( document.querySelector('.range-slider__input') ) {
             let currentLeft = Number( currentThump.style.left.replace('%', '') );
   
             // УСЛОВИЕ ДВИЖЕНИЯ ПОЛЗУНКА
-            
             if (whichThump === 'left') {
               leftThump.style.zIndex = 3;
               rightThump.style.zIndex = 2;
@@ -403,4 +465,180 @@ if ( document.querySelector('.range-slider__input') ) {
   */
   range.create(0, 20000, 5000, 10000, 100);
 
+}
+
+if ( document.querySelector('.pagination') ) {
+
+  /*
+  .pagination
+    .pagination__items
+      .pagination__back.material-icons.pagination_hidden arrow_back
+      .pagination__item.pagination__item_active 1
+      .pagination__item 2
+      .pagination__item 3
+      .pagination__item ...
+      .pagination__item 15 - всегда равна end
+      .pagination__next.material-icons arrow_forward
+    .pagination__text 1 – 12 из 100+ вариантов аренды
+
+  start = 1
+  end = 20
+  (1) 2 3 . 20
+  1 (2) 3 . 20
+  1 2 (3) . 20
+
+  */
+  let pagination = document.querySelector('.pagination');
+  let lastPage = pagination.getAttribute('pages');
+  if (lastPage < 5) {
+    lastPage = 5;
+  }
+
+  createElement('.pagination', 'div.pagination__items');
+    createElement('.pagination__items', 'div.pagination__back.material-icons.pagination_hidden', 'arrow_back');
+    createElement('.pagination__items', 'div.pagination__item.pagination__item_active', '1', 'page=1');
+    createElement('.pagination__items', 'div.pagination__item', '2', 'page=2');
+    createElement('.pagination__items', 'div.pagination__item', '3', 'page=3');
+    if (lastPage > 5) {
+      createElement('.pagination__items', 'div.pagination__item', '...', 'page=all');
+    } else {
+      createElement('.pagination__items', 'div.pagination__item', '4', 'page=4');
+    }
+    createElement('.pagination__items', 'div.pagination__item', lastPage, `page=${lastPage}`);
+    createElement('.pagination__items', 'div.pagination__next.material-icons', 'arrow_forward');
+  createElement('.pagination', 'div.pagination__text', '1 – 12 из 100+ вариантов аренды');
+
+  let paginationItem = document.querySelectorAll('.pagination__item');
+  let paginationItemLenght = paginationItem.length;
+  let i = 0;
+
+  paginationItem.forEach( (element, indx) => {
+    element.onclick = () => {
+      if (element.getAttribute('page') !== 'all' && element.getAttribute('page') !== '1') {
+        if (i === 0) {
+          paginationItem[0].classList.add('pagination_hidden');
+          back.classList.remove('pagination_hidden');
+        }
+        paginationItem.forEach( element => { 
+          element.classList.remove('pagination__item_active');
+        })
+        element.classList.add('pagination__item_active');
+        i = indx;
+        pagination.setAttribute('current-page', `${element.getAttribute('page')}`);
+      };
+    }
+  })
+
+  let back = document.querySelector('.pagination__back');
+  back.onclick = () => {
+    if (i < paginationItemLenght && i >= 0) {
+
+      if (i > 1) {
+        paginationItem[i].classList.remove('pagination__item_active');
+        paginationItem[i - 1].classList.add('pagination__item_active');
+        i--;
+      }else if (i === 1 && Number( paginationItem[i].getAttribute('page') ) > 2) {
+        paginationItem[i-1].setAttribute('page', `${Number( paginationItem[i-1].getAttribute('page') ) - 1}`);
+        paginationItem[i-1].innerHTML = paginationItem[i-1].getAttribute('page');
+
+        paginationItem[i].setAttribute('page', `${Number( paginationItem[i].getAttribute('page') ) - 1}`);
+        paginationItem[i].innerHTML = paginationItem[i].getAttribute('page');
+
+        paginationItem[i+1].setAttribute('page', `${Number( paginationItem[i+1].getAttribute('page') ) - 1}`);
+        paginationItem[i+1].innerHTML = paginationItem[i+1].getAttribute('page');
+      } else if (i === 1 && Number( paginationItem[i].getAttribute('page') ) === 2) {
+        paginationItem[i].classList.remove('pagination_hidden');
+        back.classList.add('pagination_hidden');
+        paginationItem[i].classList.remove('pagination__item_active');
+        paginationItem[i - 1].classList.add('pagination__item_active');
+        i--;
+      }
+
+      if (Number( paginationItem[i].getAttribute('page') ) === lastPage - 4 && lastPage > 5) {
+        paginationItem[i+2].setAttribute('page', 'all');
+        paginationItem[i+2].innerHTML = '...';
+      }
+
+    }
+    pagination.setAttribute('current-page', `${paginationItem[i].getAttribute('page')}`);
+  };
+  
+  let next = document.querySelector('.pagination__next');
+  next.onclick = () => {
+    if (i < paginationItemLenght-1) {
+      if (paginationItem[3].getAttribute('page') === 'all' && i === 2) {
+        i = 2;
+
+        paginationItem[i-2].setAttribute('page', `${Number( paginationItem[i-2].getAttribute('page') ) + 1}`);
+        paginationItem[i-2].innerHTML = paginationItem[i-2].getAttribute('page');
+    
+        paginationItem[i-1].setAttribute('page', `${Number( paginationItem[i-1].getAttribute('page') ) + 1}`);
+        paginationItem[i-1].innerHTML = paginationItem[i-1].getAttribute('page');
+    
+        paginationItem[i].setAttribute('page', `${Number( paginationItem[i].getAttribute('page') ) + 1}`);
+        paginationItem[i].innerHTML = paginationItem[i].getAttribute('page');
+      } else if (i < 2) {
+        paginationItem[i].classList.remove('pagination__item_active');
+        paginationItem[i+1].classList.add('pagination__item_active');
+      
+        if (i === 0) {
+          paginationItem[i].classList.add('pagination_hidden');
+          back.classList.remove('pagination_hidden');
+        }
+
+        i++;
+      } else {
+        i++;
+      }
+      
+      if (Number( paginationItem[i].getAttribute('page') ) === lastPage - 2) {
+        paginationItem[i+1].setAttribute('page', `${lastPage - 1}`);
+        paginationItem[i+1].innerHTML = paginationItem[i+1].getAttribute('page');
+      }
+
+      if (paginationItem[3].getAttribute('page') !== 'all' && (i === 3 || i === 4)) {
+        paginationItem[i-1].classList.remove('pagination__item_active');
+        paginationItem[i].classList.add('pagination__item_active');
+      }
+    }
+    pagination.setAttribute('current-page', `${paginationItem[i].getAttribute('page')}`);
+  };
+
+}
+
+function createElement(where, what, content='none', attribute='none', insert='end') {
+  /* 
+  создать элемент: 
+    где-куда?
+    какой и с каким классом?
+    с каким атрибутом?
+    с каким содержимым?
+    в начало или конец?
+  */
+  let block = document.querySelector(where);
+
+
+  let whatSplit = what.split('.');
+
+  let tag = whatSplit.shift();
+  tag = document.createElement(tag);
+  if (attribute !== 'none') {
+    let attr = attribute.split('=');
+    tag.setAttribute(attr[0], attr[1]);
+  }
+
+  let className = whatSplit.join(' ');
+  tag.className = className;
+
+
+  if (content !== 'none') {
+    tag.innerHTML = content;
+  }
+
+
+  if (insert === 'start') {
+    block.prepend(tag);
+  } else if (insert === 'end') {
+    block.append(tag);
+  }
 }
